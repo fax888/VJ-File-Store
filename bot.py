b91be6d63629fd5f9c7c8758191d478a7dd9205d
Arush <alan2.008y@gmail.com>
2025-12-25 22:34:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,8 @@ logging.getLogger("pyrogram").setLevel(logging.ERROR)
 # Don't Remove Credit Tg - @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
+pyroutils.MIN_CHAT_ID = -999999999999
+pyroutils.MIN_CHANNEL_ID = -100999999999999
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
@@ -49,7 +50,6 @@ from TechVJ.bot.clients import initialize_clients
 # Don't Remove Credit Tg - @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-pyroutils.MIN_CHANNEL_ID = -100999999999999
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
